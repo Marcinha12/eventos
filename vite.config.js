e1c70f6b4e4f697d190d1a66a037540b78f41752
Marcia <mamasuelen@gmail.com>
2024-08-21 09:21:45 -0300

    Update Vite configuration and add build assets

--- vite.config.js
+++ vite.config.js
@@ -11,4 +11,8 @@ export default defineConfig({
             refresh: true,
         }),
     ],
+    build: {
+        manifest: true,
+        outDir: 'public/build', // Diretório onde os arquivos compilados serão colocados
+    },
 });
